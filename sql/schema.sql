61e2db8c316561ec298707017a624087b07d64af
Erika <erika.paige.handley@gmail.com>
2020-05-25 14:07:20 -0700

    imported npm packages, updated schemas.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,13 +1,27 @@
-DROP DATABASE IF EXISTS
-_db;
+DROP DATABASE IF EXISTS company_db;
 
-CREATE DATABASE 
-_db;
+CREATE DATABASE company_db;
 
-USE 
-_db;
+USE company_db;
 
-CREATE TABLE tableName (
+CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
- 
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(65, 2) NOT NULL,
+    --  variable that identifies the department role
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL, 
+     -- variable that identifies the employee's manager, if they have one. Can be null
+    manager_id INT
 );
