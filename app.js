bbb23870592c87cd0418a47cb2b4571fe622c7b7
Erika <erika.paige.handley@gmail.com>
2020-05-26 21:51:23 -0700

    working exit function

--- app.js
+++ app.js
@@ -19,59 +19,58 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  // start();
+  start();
 });
-
-
 
 // console.log('This is a command line application that allows employers to manage departments, roles, and employees in their company. The application permits employers to view, add, update or delete specific data points in their company employee database.')
 
 //npm start is function to being command line
 
-//start questions to ask user
-// let start = () => {
-//   prompt([
-//     {
-//     type:'list',
-//     name:'questions',
-//     message: 'What would you like to do?',
-//     choices: ['View All Employees', 'View All Employees By Department', 'View All Employee By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manger', 'Exit']
-//     }
-//   ])
-//   .then (({ questions }) =>{ 
-//     // confirm prompts displaying /processing data 
-//     // console.log(questions)
-//     switch(questions) {
-//       case 'View All Employees':
-//         viewAllEmployees()
-//         break
-//       case 'View All Employees By Department':
-//         viewEmployeeDepartment()
-//         break
-//       case 'View All Employees by Manager':
-//         viewEmployeeManager()
-//         break
-//       case 'Add Employee':
-//         addEmployee()
-//         break
-//       case 'Remove Employee':
-//         removeEmployee()
-//         break
-//       case 'Update Employee Role':
-//         updateEmployee()
-//         break
-//       case 'Update Employee Manager':
-//         updateEmployeeManager()
-//         break
-      // case 'Exit':
-      //   exit()
-      //   break
-//     }
-//   })
-//   .catch(err => console.log(err))
-// }
+// start questions to ask user
+let start = () => {
+  prompt([
+    {
+    type:'list',
+    name:'questions',
+    message: 'What would you like to do?',
+    choices: ['View All Employees', 'View All Employees By Department', 'View All Employee By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manger', 'Exit']
+    }
+  ])
+  .then (({ questions }) =>{ 
+    // confirm prompts displaying /processing data 
+    // console.log(questions)
+    switch(questions) {
+      case 'View All Employees':
+        viewAllEmployees()
+        break
+      case 'View All Employees By Department':
+        viewEmployeeDepartment()
+        break
+      case 'View All Employees by Manager':
+        viewEmployeeManager()
+        break
+      case 'Add Employee':
+        addEmployee()
+        break
+      case 'Remove Employee':
+        removeEmployee()
+        break
+      case 'Update Employee Role':
+        updateEmployee()
+        break
+      case 'Update Employee Manager':
+        updateEmployeeManager()
+        break
+      case 'Exit':
+        console.log('exit')
+        connection.end()
+        process.exit
+        break
+    }
+  })
+  .catch(err => console.log(err))
+}
 
-// start()
 
 // let viewAllEmployees = () => {
 //   console.log('')
